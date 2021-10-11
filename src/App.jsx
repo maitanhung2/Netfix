13e7f1784d35152cb217e71bda4546838756452b
maitanhung2 <69668846+maitanhung2@users.noreply.github.com>
2021-10-11 18:54:38 +0700

    hung commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Watch from "./pages/home/watch/Watch";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+const App = () => {
+  return <Watch />;
+};
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+
+import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import List from "../../components/list/List";
+import "./home.scss";
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <Navbar />
+            <Featured />
+            <List />
+            <List />
+            <List />
+            <List />
+        </div>
+    )
+}
